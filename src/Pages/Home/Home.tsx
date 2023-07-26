03f0540ce188a61ab32aa00257e97795c69ba001
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-07-26 16:43:54 +0400

    style: built the home page

--- src/Pages/Home/Home.tsx
+++ src/Pages/Home/Home.tsx
@@ -4,16 +4,20 @@ function Home() {
   return (
     <body className="home">
       <Header />
-      <h3>SO, YOU WANT TO TRAVEL TO</h3>
-      <h1>SPACE</h1>
-      <p>
-        Let’s face it; if you want to go to space, you might as well genuinely
-        go to outer space and not hover kind of on the edge of it. Well sit
-        back, and relax because we’ll give you a truly out of this world
-        experience!
-      </p>
-      <div className="explore">
-        <p>EXPLORE</p>
+      <div className="home-divs">
+        <div className="text">
+          <h3>SO, YOU WANT TO TRAVEL TO</h3>
+          <h1>SPACE</h1>
+          <p>
+            Let’s face it; if you want to go to space, you might as well
+            genuinely go to outer space and not hover kind of on the edge of it.
+            Well sit back, and relax because we’ll give you a truly out of this
+            world experience!
+          </p>
+        </div>
+        <div className="explore">
+          <p>EXPLORE</p>
+        </div>
       </div>
     </body>
   );
