a0c1dc5849646aac0ac35ce95eddfd3ce9921249
elizabet-bagdasaryan <elizabet.bagdasaryan1@gmail.com>
2023-07-27 00:19:56 +0400

    style: built destinations page

--- src/Pages/Destination/Destionation.tsx
+++ src/Pages/Destination/Destionation.tsx
@@ -1,20 +1,45 @@
 import "./Destination.css";
-
+import Header from "../../Components/Header/Header";
+import Moon from "../../assets/moon.png";
 function Destination() {
   return (
-    <div className="destination">
-      <h3>SO, YOU WANT TO TRAVEL</h3>
-      <h1>SPACE</h1>
-      <p>
-        Let’s face it; if you want to go to space, you might as well genuinely
-        go to outer space and not hover kind of on the edge of it. Well sit
-        back, and relax because we’ll give you a truly out of this world
-        experience!
-      </p>
-      <div className="explore">
-        <p>EXPLORE</p>
+    <body className="destination">
+      <Header />
+      <div className="destination-divs">
+        <div>
+          <h3>
+            <p>01</p>Pick your destination
+          </h3>
+          <img src={Moon}></img>
+        </div>
+        <div>
+          <div className="navbar-destination">
+            <p>MOON</p>
+            <p>MARS</p>
+            <p>EUROPA</p>
+            <p>TITAN</p>
+          </div>
+          <h1>MOON</h1>
+          <p className="text-dest">
+            See our planet as you’ve never seen it before. A perfect relaxing
+            trip away to help regain perspective and come back refreshed. While
+            you’re there, take in some history by visiting the Luna 2 and Apollo
+            11 landing sites.
+          </p>
+          <hr />
+          <div className="statistics">
+            <div>
+              <p>AVG. DISTANCE</p>
+              <p>384,400 km</p>
+            </div>
+            <div>
+              <p>EST. TRAVEL TIME</p>
+              <p>3 DAYS</p>
+            </div>
+          </div>
+        </div>
       </div>
-    </div>
+    </body>
   );
 }
 
